fc0f88bb7b7d617b259ef63b125752861877b20a
saitejayeniganti <42708541+saitejayeniganti@users.noreply.github.com>
2022-09-22 15:55:41 -0700

    Login UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,20 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
+import Navigator from "./components/Navigator";
 import AdminHome from "./views/admin/adminHome";
 import CustomerHome from "./views/customer/customerHome";
 import Home from "./views/Home";
+import Login from "./views/login";
 import MerchantHome from "./views/merchant/merchantHome";
 
 function App() {
   return (
     <div className="App">
      <Router>
+     <Navigator />
      <Routes>
      <Route path="/home" exact element={<Home />}></Route>
+     <Route path="/login" exact element={<Login />}></Route>
      <Route path="/customerhome" exact element={<CustomerHome />}></Route>
      <Route path="/merchanthome" exact element={<MerchantHome />}></Route>
      <Route path="/adminhome" exact element={<AdminHome />}></Route>
