e7332df2cf16b37a0beecf2e0788bdb6b022ffd1
Anil Gubbala <anil.gubbala@sjsu.edu>
2022-11-15 13:27:41 -0800

    search input based vendors

--- backend/routes/Home/customer.js
+++ backend/routes/Home/customer.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 const MerchantModel = require("../../model/merchant");
 
-router.get("/merchants", (req, res) => {
-  console.log(req.query);
-  const location = JSON.parse(req.query.location);
-  const lat = location.coordinates[1];
-  const lng = location.coordinates[0];
+const doLocationFilter = (lng, lat, ids, res) => {
+  let query = {};
+  if (ids) {
+    query = { _id: { $in: ids.map((each) => each._id) } };
+  }
   MerchantModel.aggregate([
     {
       $geoNear: {
@@ -15,9 +15,10 @@ router.get("/merchants", (req, res) => {
           coordinates: [lng, lat],
         },
         distanceField: "dist.calculated",
-        maxDistance: 33000,
+        maxDistance: 66000,
         includeLocs: "dist.location",
         spherical: true,
+        query: query,
       },
     },
   ])
@@ -27,6 +28,45 @@ router.get("/merchants", (req, res) => {
     .catch((err) => {
       res.status(500).send({});
     });
+};
+
+router.get("/merchants", (req, res) => {
+  // console.log(req.query);
+  const location = JSON.parse(req.query.location);
+  const lat = location.coordinates[1];
+  const lng = location.coordinates[0];
+  const searchInput = req.query.searchInput;
+
+  if (searchInput) {
+    MerchantModel.aggregate([
+      {
+        $search: {
+          index: "merchantSearch",
+          autocomplete: {
+            query: searchInput,
+            path: "storeName",
+          },
+        },
+      },
+      {
+        $project: {
+          _id: 1,
+        },
+      },
+    ])
+      .then((response) => {
+        // console.log(response);
+        doLocationFilter(lng, lat, response, res);
+        // res.status(200).send(response);
+      })
+      .catch((error) => {
+        console.log(error);
+        res.sendStatus(500);
+      });
+  } else {
+    doLocationFilter(lng, lat, "", res);
+  }
+
   // res.send();
 });
 
