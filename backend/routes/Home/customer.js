195b8fe8ecf0e794794f320def7db6bdb47a4d3a
Anil Gubbala <anil.gubbala@sjsu.edu>
2022-11-18 01:05:41 -0800

    multisearch added.

--- backend/routes/Home/customer.js
+++ backend/routes/Home/customer.js
@@ -24,7 +24,7 @@ const doLocationFilter = (lng, lat, ids, res) => {
     },
   ])
     .then((result) => {
-      res.status(200).send({ vendors: result });
+      res.status(200).send({ vendorDetails: result });
     })
     .catch((err) => {
       res.status(500).send({});
@@ -70,6 +70,119 @@ router.get("/merchants", (req, res) => {
 
   // res.send();
 });
+
+const doMerchantSearch = (lat, lng, searchInput, res) => {
+  if (searchInput) {
+    MerchantModel.aggregate([
+      {
+        $search: {
+          index: "merchantSearch",
+          autocomplete: {
+            query: searchInput,
+            path: "storeName",
+          },
+        },
+      },
+      {
+        $project: {
+          _id: 1,
+        },
+      },
+    ])
+      .then((vendors) => {
+        // console.log(response);
+        // doLocationFilter(lng, lat, response, res);
+        doProductSearch(lat, lng, searchInput, vendors, res);
+        // res.status(200).send(response);
+      })
+      .catch((error) => {
+        console.log(error);
+        res.sendStatus(500);
+      });
+  } else {
+    doLocationFilter(lng, lat, "", res);
+  }
+};
+
+const doProductSearch = (lat, lng, searchInput, vendors, res) => {
+  ProductModel.aggregate([
+    {
+      $search: {
+        index: "productSearch",
+        autocomplete: {
+          query: searchInput,
+          path: "name",
+        },
+      },
+    },
+    {
+      $project: {
+        _id: 1,
+        name: 1,
+        merchant: 1,
+      },
+    },
+  ])
+    .then((products) => {
+      // console.log(response);
+      doCombinedLocationSearch(lat, lng, products, vendors, res);
+      // res.status(200).send(response);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.sendStatus(500);
+    });
+};
+
+const doCombinedLocationSearch = (lat, lng, products, vendors, res) => {
+  let query = {};
+  // let productsMap = {};
+  const merchantSet = new Set();
+  const vendorOnly = [];
+  products.forEach((each) => {
+    merchantSet.add(each.merchant);
+    // productsMap[element._id] = { name: element.name , };
+  });
+  vendors.forEach((each) => {
+    if (!merchantSet.has(each._id)) {
+      vendorOnly.push(each._id);
+      merchantSet.add(each._id);
+    }
+  });
+  const merchantArray = Array.from(merchantSet);
+  if (merchantArray) {
+    query = { _id: { $in: merchantArray.map((each) => each._id) } };
+  }
+  MerchantModel.aggregate([
+    {
+      $geoNear: {
+        near: {
+          type: "Point",
+          coordinates: [lng, lat],
+        },
+        distanceField: "dist.calculated",
+        maxDistance: 66000,
+        includeLocs: "dist.location",
+        spherical: true,
+        query: query,
+      },
+    },
+  ])
+    .then((result) => {
+      let merchantMap = {};
+      result.forEach((each) => {
+        merchantMap[each._id] = each;
+      });
+      res.status(200).send({
+        productVendors: products,
+        vendorList: vendorOnly,
+        vendorDetails: result,
+      });
+    })
+    .catch((err) => {
+      res.status(500).send({});
+    });
+};
 
 const doProductLocationFilter = (lng, lat, products, res) => {
   let query = {};
@@ -151,5 +264,37 @@ router.get("/products", (req, res) => {
 
   // res.send();
 });
+
+router.get("/multiSearch", (req, res) => {
+  const location = JSON.parse(req.query.location);
+  const lat = location.coordinates[1];
+  const lng = location.coordinates[0];
+  const searchInput = req.query.searchInput;
+  doMerchantSearch(lat, lng, searchInput, res);
+});
+
+function task1() {
+  return new Promise((resolve, reject) => {
+    const count = 100000000;
+    let i = 0;
+    while (i < count) {
+      i++;
+    }
+    resolve("task1 is done");
+  });
+}
+function task2() {
+  return new Promise((resolve, reject) => {
+    resolve("task2 is done");
+  });
+}
+const tasks = [task1(), task2()]; //add tasks here
+Promise.all(tasks)
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
 
 module.exports = router;
